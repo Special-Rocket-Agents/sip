e83353b37ab62c5f4b375e05a3db9d73180d3b75
arezg <aradytfa@gmail.com>
2024-03-16 10:14:44 +0330

    Update sip.py

--- sip.py
+++ sip.py
@@ -1,15 +1,25 @@
 # sip, the package installer
 import os
+import sys
 import urllib.request
 import main
+from main import *
 
 def install_package(pkname, dir, username):
     try:
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/Special-Rocket-Agents/sip/main/" + pkname + ".py", dir + "/user/" + username + "/pkg/" + pkname + ".py")
+        paakname = urllib.request.urlretrieve("https://raw.githubusercontent.com/Special-Rocket-Agents/sip/main/" + pkname + ".py", osdir + "/user/" + username + "/pkg/" + pkname + ".py")
         print(f"Package '{pkname}' installed successfully!")
     except Exception as e:
         print(f"Error installing package '{pkname}': {e}")
 
-if __name__ == "__main__":
-    pkname = input("Enter the name of the package to install: ")
-    install_package(pkname, dir, username)
+def run(importantFunctions, importantVars):
+    pakname = args[1]
+    if pakname == "debug":
+        print(f"OSDir: {osdir}")
+        print(f"username: {username}")
+        print(f"packagecount: {packagecount}")
+        print(f"curdir: {curdir}")
+        print(f"displayusername: {displayusername}")
+    else:
+        install_package(pakname, curdir, displayusername)
+    return
