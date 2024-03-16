3ea769ad00c4030dd0513d5cdfa207f705734896
arezg <aradytfa@gmail.com>
2024-03-16 09:20:39 +0330

    Update calc.py

--- calc.py
+++ calc.py
@@ -12,25 +12,25 @@ def divide(x, y):
         return "Error! Division by zero!"
     else:
         return x / y
+def run(importantFunctions, importantVars):
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
 
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+    choice = input("Enter choice (1/2/3/4): ")
 
-choice = input("Enter choice (1/2/3/4): ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-if choice == '1':
-    print("Result:", add(num1, num2))
-elif choice == '2':
-    print("Result:", subtract(num1, num2))
-elif choice == '3':
-    print("Result:", multiply(num1, num2))
-elif choice == '4':
-    print("Result:", divide(num1, num2))
-else:
-    print("Invalid input")
+    if choice == '1':
+        print("Result:", add(num1, num2))
+    elif choice == '2':
+        print("Result:", subtract(num1, num2))
+    elif choice == '3':
+        print("Result:", multiply(num1, num2))
+    elif choice == '4':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
